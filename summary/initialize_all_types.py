44254e26454994e99a5b61498171f176b9c86cd8
omnidox <hidalgor@montclair.edu>
2024-09-17 20:44:49 -0400

    finalizing_files
    finalizing_files

---
+++ summary/initialize_all_types.py
@@ -0,0 +1,32 @@
+print()
+
+# Initializing a string
+my_string = "Hello, world!"  # A sequence of characters used for text
+
+# Initializing a list
+my_list = [1, 2, 3, 4, 5]  # A mutable (changeable) container with ordered elements
+
+# Initializing a tuple
+my_tuple = (10, 20, 30)  # An immutable (unchangeable) container with ordered elements
+
+# Initializing a set
+my_set = {1, 2, 3, 4, 5}  # A mutable container with unordered and unique elements
+
+my_set2 = set([1, 2, 3, 4, 5])  # A mutable container with unordered and unique elements
+
+# Initializing a dictionary
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}  # A mapping type container with key-value pairs
+
+# Printing all initialized data types
+print("String example:", my_string)
+print()
+print("List example:", my_list)
+print()
+print("Tuple example:", my_tuple)
+print()
+print("Set curly braces example:", my_set)
+print()
+print("Set function example:", my_set2)
+print()
+print("Dictionary example:", my_dict)
+print()
---
+++ summary/initialize_all_types_empty.py
@@ -0,0 +1,29 @@
+
+
+print()
+# Initializing an empty string
+empty_string = ""  # An empty string contains no characters
+
+# Initializing an empty list
+empty_list = []  # An empty list is a mutable container with no elements
+
+# Initializing an empty tuple
+empty_tuple = ()  # An empty tuple is an immutable container with no elements
+
+# Initializing an empty set
+empty_set = set()  # An empty set is a mutable container with no elements; note: you cannot use {} as it creates an empty dictionary
+
+# Initializing an empty dictionary
+empty_dict = {}  # An empty dictionary is a container with no key-value pairs
+
+# Printing all initialized empty data types
+print("Empty string:", empty_string)
+print()
+print("Empty list:", empty_list)
+print()
+print("Empty tuple:", empty_tuple)
+print()
+print("Empty set:", empty_set)
+print()
+print("Empty dictionary:", empty_dict)
+print()
